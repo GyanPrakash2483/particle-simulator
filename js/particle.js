7c00db94cfd9c93679e17b9267a1decad63d9587
GyanPrakash2483 <69905535+GyanPrakash2483@users.noreply.github.com>
2022-01-26 20:45:18 +0530

    Add files via upload

---
+++ js/particle.js
@@ -0,0 +1,13 @@
+class particle {
+	constructor(mass, charge, size, color, posX, posY, vX, vY, isLocked) {
+		this.mass = mass;
+		this.charge = charge;
+		this.size = size;
+		this.color = color;
+		this.posX = posX;
+		this.posY = posY;
+		this.vX = vX;
+		this.vY = vY;
+		this.isLocked = isLocked;
+	}
+}
